f7574fe1a2fb726367b44c785f33f38b33520579
Adarsh Jain <adarsh.jain428@gmail.com>
2026-03-10 13:43:42 +0530

    giithub repo file removal

--- github.tf
+++ github.tf
@@ -1,27 +1,27 @@
-terraform {
-  required_providers {
-    github = {
-      source  = "integrations/github"
-      version = "~> 6.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     github = {
+#       source  = "integrations/github"
+#       version = "~> 6.0"
+#     }
+#   }
+# }
 
-# GitHub token is injected via TF_VAR_github_token (set as GitHub Actions Secret: GITHUB_TOKEN_TF)
-variable "github_token" {
-  description = "GitHub Personal Access Token"
-  type        = string
-  sensitive   = true
-}
+# # GitHub token is injected via TF_VAR_github_token (set as GitHub Actions Secret: GITHUB_TOKEN_TF)
+# variable "github_token" {
+#   description = "GitHub Personal Access Token"
+#   type        = string
+#   sensitive   = true
+# }
 
-# Configure the GitHub Provider
-provider "github" {
-  token = var.github_token
-}
+# # Configure the GitHub Provider
+# provider "github" {
+#   token = var.github_token
+# }
 
-resource "github_repository" "example" {
-  name        = "example"
-  description = "My awesome codebase"
+# resource "github_repository" "example" {
+#   name        = "example"
+#   description = "My awesome codebase"
 
-  visibility = "public"
-}
+#   visibility = "public"
+# }
